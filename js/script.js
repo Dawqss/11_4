b756137cd392946c90e370e4f8de17030a51a6b6
Dawid Wyrkowski <dawidwyrkowski@gmail.com>
2016-11-21 01:30:05 +0100

    #1 issue

--- js/script.js
+++ js/script.js
@@ -3,16 +3,15 @@ var url = 'http://api.icndb.com/jokes/random',
 	paragraph = document.getElementById('joke'),
 	ident = document.getElementById('identification');
 
-button.addEventListener('click', function(){getJoke();});
+button.addEventListener('click', getJoke);
 
 function getJoke() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.addEventListener('load', function(){
-    var response = JSON.parse(xhr.response);
-    paragraph.innerText = response.value.joke;
-    ident.innerText = 'joke id: ' +response.value.id;
-
+    var response = JSON.parse(xhr.response).value;
+    paragraph.innerText = response.joke;
+    ident.innerText = 'joke id: ' +response.id;
   });
   xhr.send();
 }
